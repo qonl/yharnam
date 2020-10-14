a84ed825eda88a775ecbc4e0c0ecb67e5ba5d19b
Quinten O'Neal <quintenoneal@Quintens-MacBook-Pro.local>
2020-10-13 23:50:07 -0400

    add try catch to products endpoint

--- pages/api/products.js
+++ pages/api/products.js
@@ -1,6 +1,10 @@
 import { getAllProducts } from '../../lib/api';
 
 export default async function getProducts(req, res) {
-    const data = await getAllProducts();
-    res.status(200).json(data);
+    try {
+        const data = await getAllProducts();
+        res.status(200).json(data);
+    } catch(error) {
+        throw new Error(error);
+    }
 }
